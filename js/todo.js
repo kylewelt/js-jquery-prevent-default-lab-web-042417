ca61f56ecd8433650ff6bd2517a1c53b1664a474
kylewelt <kylewelt@umich.edu>
2017-06-09 15:19:51 -0400

    Done.

--- js/todo.js
+++ js/todo.js
@@ -1,5 +1,16 @@
 $(document).ready(function(){
   // call functions here
-});
+  addItem()
+})
 
 // define functions here
+function addItem () {
+  $('form').submit(function (e) {
+    var $list = $('#list ol'),
+    $todo = $('#item').val()
+
+    $list.append(`<li>${$todo}</li>`)
+    $('form').trigger('reset')
+    e.preventDefault()
+  })
+}
